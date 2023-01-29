ea672c4030e9f8e4b72d6457bd7b14f80a1df468
tutorcs <tutorcs@163.com>
2023-01-29 20:05:56 +0800

    WeChat cstutorcs CS412 Introduction To Data Mining HW4

---
+++ homework4_q1.py
@@ -0,0 +1,13 @@
+https://tutorcs.com
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+def my_Bayes_candy(pi_list, p_list, c_list):
+    
+    posterior_probabilities = [[0]*5 for i in range(10)] # Default initialization with 0s.
+
+    '''
+        Implement your code to calculate the posterior probabilities here.
+    '''
+
+    return posterior_probabilities
---
+++ homework4_q2.py
@@ -0,0 +1,21 @@
+https://tutorcs.com
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+import numpy as np
+import random
+import math
+from xgboost import XGBClassifier
+from sklearn.svm import SVC, LinearSVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+
+def my_train_test(method,X,y,pi,k):
+    
+    errors = []
+
+    '''
+        Implement your code to calculate the errors here.
+    '''
+
+    return np.array(errors)
